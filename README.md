405cf9da7fffdfe3414bd612206b332e56d32e98
Yu Watanabe <yunabe.public@gmail.com>
2019-05-29 13:03:43 +0900

    Revert to gatsby version.

--- README.md
+++
@@ -1,5 +0,0 @@
-# Update yunabe.jp
-
-```shell
-jekyll clean && jekyll build && rm -rf ~/src/yunabe.github.io/* && cp -r _site/* ~/src/yunabe.github.io/
-```
---
+++ styles-6dc9e1bb46ccd13e3789.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{140:function(n,o,w){},141:function(n,o,w){}}]);
+//# sourceMappingURL=styles-6dc9e1bb46ccd13e3789.js.map
